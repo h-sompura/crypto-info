e12d2e4c4105245e688e06ac50f45759c537dc92
Harshitha Sompura <sompharshi123@gmail.com>
2022-12-16 01:33:55 -0500

    Revert "Homepage setup 🍊 (#5)"
    This reverts commit ff7723739813dcd4b9cf1cdc9c8cadc4d9c1c5ba.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-// React Imports
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Style Imports
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/GlobalStyle";
 import { theme } from "./styles/theme";
-// Page Imports
-import Home from "./pages/Home";
-
+// Component Imports
+import Heading from "./components/Heading";
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      {/* Routes Setup */}
-      <Router>
-        <Routes>
-          <Route element={<Home />} path="/" />
-        </Routes>
-      </Router>
+      <Heading>Hello, World!</Heading>
     </ThemeProvider>
   );
 }

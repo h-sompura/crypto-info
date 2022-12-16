6314ad4f2b840fd01a8134f7562d3e6ac4c685c9
h-sompura <sompharshi123@gmail.com>
2022-12-16 01:53:37 -0500

    Updated Heading Homepage 🗾

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -2,6 +2,10 @@ import styled from "styled-components";
 const Heading = styled.h1`
   font-family: ${(props) => ` ${props.theme.fonts.unbounded}, cursive`};
   color: ${(props) => props.theme.colors.primaryCyan};
+  font-size: ${(props) => props.theme.fontSizes.medium};
+  padding:.5em 2em;
+  letter-spacing: .05em;
+  line-height:1.5em;
 `;
 
 export default Heading;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 // Component Imports
 import Heading from "../components/Heading";
 const Home = () => {
-  return <Heading>Home</Heading>;
+  return <Heading>Crypto Info</Heading>;
 };
 
 export default Home;

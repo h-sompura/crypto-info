01dccd39f9eb88b42708507c56591feae80c3738
h-sompura <sompharshi123@gmail.com>
2022-12-15 21:53:08 -0500

    Created GlobalStyles 💌

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from "styled-components";
+const GlobalStyle = createGlobalStyle`
+    body{
+        font-family: "Nunito Sans", sans-serif;
+    }
+`;
+
+export default GlobalStyle;

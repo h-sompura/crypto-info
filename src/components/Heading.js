b1feab2754e0ad97720345b0e0d8883ea39aa607
h-sompura <sompharshi123@gmail.com>
2022-12-16 02:21:45 -0500

    Updated Heading component 🚧

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -3,9 +3,9 @@ const Heading = styled.h1`
   font-family: ${(props) => ` ${props.theme.fonts.unbounded}, cursive`};
   color: ${(props) => props.theme.colors.primaryCyan};
   font-size: ${(props) => props.theme.fontSizes.medium};
-  padding:.5em 2em;
-  letter-spacing: .05em;
-  line-height:1.5em;
+  padding: 0.5em 0;
+  letter-spacing: 0.05em;
+  line-height: 1.5em;
 `;
 
 export default Heading;

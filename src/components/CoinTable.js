75608fe646c902399a1b02bffa5718785df15a7b
h-sompura <sompharshi123@gmail.com>
2022-12-16 23:52:17 -0500

    API Fetch Tested and Completed 🥨

--- src/components/CoinTable.js
+++ src/components/CoinTable.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Icon from "@mui/material/Icon";
 import { DataGrid } from "@mui/x-data-grid";
 import Container from "./Container";
@@ -51,93 +51,81 @@ const columns = [
   },
 ];
 
-const rows = [
-  {
-    id: 1,
-    name: "Bitcoin",
-    price: "$17471.709299369093",
-    twentyFourh: "-1.36%",
-    marketCap: "335859750454",
-  },
-  {
-    id: 2,
-    name: "Ethereum",
-    price: "$17471.709299369093",
-    twentyFourh: "-1.36%",
-    marketCap: "335859750454",
-  },
-  {
-    id: 3,
-    name: "Tether USD",
-    price: "$17471.709299369093",
-    twentyFourh: "-1.36%",
-    marketCap: "335859750454",
-  },
-  {
-    id: 4,
-    name: "USDC",
-    price: "$17471.709299369093",
-    twentyFourh: "-1.36%",
-    marketCap: "335859750454",
-  },
-  {
-    id: 5,
-    name: "BNB",
-    price: "$17471.709299369093",
-    twentyFourh: "-1.36%",
-    marketCap: "335859750454",
-  },
-  {
-    id: 6,
-    name: "XRP",
-    price: "$17471.709299369093",
-    twentyFourh: "-1.36%",
-    marketCap: "335859750454",
-  },
-  {
-    id: 7,
-    name: "Binance USD",
-    price: "$17471.709299369093",
-    twentyFourh: "-1.36%",
-    marketCap: "335859750454",
-  },
-  {
-    id: 8,
-    name: "Dogecoin",
-    price: "$17471.709299369093",
-    twentyFourh: "-1.36%",
-    marketCap: "335859750454",
-  },
-  {
-    id: 9,
-    name: "Cardano",
-    price: "$17471.709299369093",
-    twentyFourh: "-1.36%",
-    marketCap: "335859750454",
-  },
-];
-
 const CoinTable = () => {
+  // State Variables
+  const [loading, setLoading] = useState(true);
+  const [tableData, setTableData] = useState([]);
+
+  const apiURL = process.env.REACT_APP_API_URL;
+  const proxyURL = process.env.REACT_APP_PROXY_URL;
+
+  // UseEffect Hook
+  useEffect(() => {
+    fetchCoinData();
+  });
+
+  //Helpers/methods
+  const fetchCoinData = async () => {
+    const resultsList = [];
+    try {
+      const responseFromAPI = await fetch(`${proxyURL}${apiURL}`, {
+        method: "GET",
+        headers: {
+          "Content-type": "application-json",
+          "x-access-token": process.env.REACT_APP_CRYPTO_API_KEY,
+          "Access-Control-Allow-Origin": "*",
+        },
+      });
+      if (responseFromAPI.ok) {
+        setLoading(false);
+        const jsonData = await responseFromAPI.json();
+        console.log(`Data received from API: ${jsonData.data}`);
+        resultsList.push(jsonData.data.coins);
+      }
+    } catch (err) {
+      setLoading(true);
+      console.log(`Error while fetching data from the API: ${err}`);
+    }
+    const coinsData = resultsList[0].map((result) => ({
+      id: result.uuid,
+      price: result.price,
+      twentyFourh: result.change,
+      marketCap: result.marketCap,
+      name: result.name,
+      icon: result.iconUrl,
+    }));
+    console.log(coinsData);
+
+    setTableData(coinsData);
+  };
+
   return (
-    <Container style={{ height: 600 }} backgroundColor="white">
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={9}
-        rowsPerPageOptions={[9]}
-        sx={{
-          fontFamily: "inherit",
-          boxShadow: 2,
-          border: 2,
-          borderColor: "primary.light",
-          "& .MuiDataGrid-columnSeparator": {
-            display: "none",
-          },
-          "& .MuiDataGrid-cell:hover": {
-            color: "primary.main",
-          },
-        }}
-      />
+    <Container style={{ height: 600, color: "white" }}>
+      {!loading && (
+        <DataGrid
+          rowHeight={70}
+          rows={tableData}
+          columns={columns}
+          pageSize={9}
+          rowsPerPageOptions={[9]}
+          sx={{
+            color: "white",
+            fontFamily: "inherit",
+            boxShadow: 2,
+            border: 0,
+            "& .MuiDataGrid-columnSeparator": {
+              display: "none",
+            },
+            "& .MuiDataGrid-cell:hover": {
+              color: "#00DAA7",
+            },
+            "& .MuiTablePagination-root": {
+              color: "#fff",
+            },
+          }}
+        />
+      )}
+      {loading && <p>💬 Loading Coins...</p>}
     </Container>
   );
 };
